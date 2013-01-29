737a7cd8ab3aa4046bbc9b7d240c458de07a11a6
Terry May <terry@detroitlabs.com>
2013-01-29 01:47:18 -0500

    adding android project to repo.

---
+++ android-thermodroid/src/com/example/thermodroid/EEPROMData.java
@@ -0,0 +1,96 @@
+package com.example.thermodroid;
+
+import java.util.TreeMap;
+
+
+public class EEPROMData {
+
+    //VTH0 of absolute temperature sensor
+    //calculated from (Vth_H:0xDB(219) & Vth_L:0xDA(218))
+    final private static int Vth_H = 0xD8;
+    final private static int Vth_L = 0xDA;
+    //KT1 of absolute temperature sensor
+    //calculated from (Kt1_H:0xDD(221) & Kt1_L:0xDC(220))
+    final private static int Kt1_H = 0xDD;
+    final private static int Kt1_L = 0xDC;
+    //KT2 of absolute temperature sensor
+    //calculated from (Kt2_H:0xDF(223) & Kt2_L:0xDE(222))
+    final private static int Kt2_H = 0xDF;
+    final private static int Kt2_L = 0xDE;
+
+    
+    private TreeMap<Integer, Integer> data;
+    
+    public EEPROMData() {
+      data = new TreeMap<Integer, Integer>();    
+    }
+
+    //TODO: check datasheet to verify int is a good type to use here
+    public void addRegister(int address, int value) {
+        data.put(address, value);
+    }
+    
+    public void getRegister(int address) {
+        data.get(address);
+    }
+    
+    public int getV_th() {
+        return (data.get(Vth_H) <<8) + data.get(Vth_L);
+    }
+    
+    public double getK_t1() {
+        return (data.get(Kt1_H) <<8) + data.get(Kt1_L)/1024.0;
+    }
+    
+    public double getK_t2() {
+        return (data.get(Kt2_H) <<8) + data.get(Kt2_L)/1048576.0;
+    }
+    
+    //(0xD4:212)Compensation pixel individual offset coefficients
+    public int getA_cp() {
+        int a_cp = data.get(212);
+        return (a_cp > 127) ? a_cp - 256 : a_cp;
+    }
+    
+    //(0xD5:213)Individual Ta dependence (slope) of the compensation pixel offset
+    public int getB_cp() {
+        int b_cp = data.get(213);
+        return (b_cp > 127) ? b_cp - 256 : b_cp;
+    }
+    
+  //(0xD8:216)Thermal gradient coefficient
+    public int getTGC() {
+      int tgc = data.get(216);
+      return (tgc > 127) ? tgc - 256 : tgc;
+    }
+    
+    //(0xD9:217)Scaling coefficient for slope of IR pixels offset
+    public int getB_I_Scale() {
+        //unsigned
+        return data.get(217);
+    }
+    
+    public double getEmissivity() {
+        return (data.get(229) <<8) + data.get(228)/32768.0;
+    }
+    
+    //(0x00...0x3F:0...63)IR pixel individual offset coefficient
+    public int[] getA_IJ() {
+      int[] a_ij = new int[64];
+      for (int i=0; i<=63;i++) {
+          int b = data.get(i);
+          a_ij[i] = (b > 127) ? b - 256 : b;
+      }
+      return a_ij;
+    }
+    
+    //(0x40...0x7F:64...127)Individual Ta dependence (slope) of IR pixels offset
+    public int[] getB_IJ() {
+        int[] b_ij = new int[64];
+        for (int i=64; i<=127;i++) {
+            int b = data.get(i);
+            b_ij[i] = (b > 127) ? b - 256 : b;
+        }
+        return b_ij;
+      }
+}
---
+++ android-thermodroid/src/com/example/thermodroid/MainActivity.java
@@ -0,0 +1,257 @@
+package com.example.thermodroid;
+
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbAccessory;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements Runnable {
+
+    final private static byte DUMP_EEPROM   =   100;
+    
+    private UsbManager mUsbManager;
+    
+    private static final String ACTION_USB_PERMISSION = "com.google.android.DemoKit.action.USB_PERMISSION";
+    private PendingIntent mPermissionIntent;
+    private UsbAccessory mAccessory;
+    private ParcelFileDescriptor mFileDescriptor;
+    private FileInputStream mInputStream;
+    private FileOutputStream mOutputStream;
+
+    private boolean mPermissionRequestPending;
+    
+    private TextView output;
+    private String outputBuffer = "";
+
+    private int mCommand;
+    
+    private EEPROMData eepromData;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        output = (TextView) findViewById(R.id.output);
+        updateOutput("startup\n");
+        eepromData = new EEPROMData();
+        initUSB();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    @Override
+    public Object onRetainNonConfigurationInstance() {
+        if (mAccessory != null) {
+            return mAccessory;
+        } else {
+            return super.onRetainNonConfigurationInstance();
+        }
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+ 
+ 
+        if (mInputStream != null && mOutputStream != null) {
+            //streams were not null");
+            return;
+        }
+        //streams were null");
+        UsbAccessory[] accessories = mUsbManager.getAccessoryList();
+        UsbAccessory accessory = (accessories == null ? null : accessories[0]);
+        if (accessory != null) {
+            if (mUsbManager.hasPermission(accessory)) {
+                openAccessory(accessory);
+            } else {
+                synchronized (mUsbReceiver) {
+                    if (!mPermissionRequestPending) {
+                        mUsbManager.requestPermission(accessory, mPermissionIntent);
+                        mPermissionRequestPending = true;
+                    }
+                }
+            }
+        } else {
+            // null accessory
+        }
+    }
+    
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+ 
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(mUsbReceiver);
+        super.onDestroy();
+    }
+    
+    private void initUSB() {
+        outputBuffer = "";
+        updateOutput("init usb");
+        mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
+        registerReceiver(mUsbReceiver, filter);
+    }
+
+    private void openAccessory(UsbAccessory accessory) {
+        updateOutput("openaccessory");
+        
+        mFileDescriptor = mUsbManager.openAccessory(accessory);
+        if (mFileDescriptor != null) {
+            mAccessory = accessory;
+            FileDescriptor fd = mFileDescriptor.getFileDescriptor();
+            mInputStream = new FileInputStream(fd);
+            mOutputStream = new FileOutputStream(fd);
+            Thread thread = new Thread(null, this, "ThermoDroid");
+            thread.start();
+            updateOutput("File Descriptor is not null");
+            sendCommand(DUMP_EEPROM, (byte) 2 , 3);
+        } else {
+            
+            updateOutput("File Descriptor is null");
+        }
+    }
+    
+    private void closeAccessory() {
+        try {
+            if (mFileDescriptor != null) {
+                mFileDescriptor.close();
+                updateOutput("Closed");
+            }
+        } catch (IOException e) {
+        } finally {
+            mFileDescriptor = null;
+            mAccessory = null;
+        }
+    }
+    
+    
+    public void run() {
+        int ret = 0;
+        byte[] buffer = new byte[16384];
+        int i;
+
+        while (ret >= 0) {
+            try {
+                ret = mInputStream.read(buffer);
+            } catch (IOException e) {
+                updateOutput("IO EXCEPTION");
+                updateOutput(e.getMessage());
+                break;
+                
+            }
+
+            i = 0;
+            while (i < ret) {
+                int len = ret - i;
+                switch(mCommand) {
+                  case DUMP_EEPROM:
+                      int value = (int)buffer[i];
+                      //updateOutput(Integer.toHexString(i)+":"+Integer.toString(value));
+                      eepromData.addRegister(i, value);
+                      break;
+                }
+                i += 1;
+                /*
+                if (len >= 1) {
+                    Message m = Message.obtain(mHandler);
+                    int value = (int)buffer[i];
+                    //m.obj = new EEPROMData("f", value);
+                    updateOutput(Integer.toHexString(i)+":"+Integer.toString(value));
+                    //mHandler.sendMessage(m);
+                }
+                */
+                //i += 1; num of bytes from arduino
+            }
+
+        }
+    }
+
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            EEPROMData data = (EEPROMData) msg.obj;
+            //Log.i("DATA", t.getReading());
+            //updateOutput(Integer.toString(data.getReading()));
+        }
+    };
+    
+    public void sendCommand(byte command, byte target, int value) {
+        mCommand = command;
+        byte[] buffer = new byte[3];
+        if (value > 255)
+            value = 255;
+
+        buffer[0] = command;
+        buffer[1] = target;
+        buffer[2] = (byte) value;
+        if (mOutputStream != null && buffer[1] != -1) {
+            try {
+                mOutputStream.write(buffer);
+            } catch (IOException e) {
+                updateOutput("send command failed");
+            }
+        }
+    }
+    
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            updateOutput("ask perms");
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbAccessory accessory = (UsbAccessory) intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        updateOutput("open accessory");
+                        openAccessory(accessory);
+                    } else {
+                        updateOutput("USB PERMS DENIED\n\n");
+                    }
+                }
+            } else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
+                UsbAccessory accessory = (UsbAccessory) intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+                if (accessory != null && accessory.equals(mAccessory)) {
+                    updateOutput("close accessory");
+                    closeAccessory();
+                }
+            }
+        }
+    };
+  
+    private void updateOutput(final String msg) {
+        runOnUiThread(new Runnable() {
+            
+            @Override
+            public void run() {
+                outputBuffer += msg + "\n";
+                output.setText(outputBuffer);                
+            }
+        });
+        
+    }
+}
